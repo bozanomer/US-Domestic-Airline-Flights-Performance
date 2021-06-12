bba1aea55b45d75e904d4e374d797079baa17d42
Ömer Bozan <bozan.omer@gmail.com>
2021-06-12 13:56:29 +0300

    Add files via upload

---
+++ dash8.py
@@ -0,0 +1,236 @@
+# Import required libraries
+import pandas as pd
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output, State
+from jupyter_dash import JupyterDash
+import plotly.graph_objects as go
+import plotly.express as px
+from dash import no_update
+
+
+# Create a dash application
+app = JupyterDash(__name__)
+JupyterDash.infer_jupyter_proxy_config()
+
+# REVIEW1: Clear the layout and do not display exception till callback gets executed
+app.config.suppress_callback_exceptions = True
+
+# Read the airline data into pandas dataframe
+airline_data =  pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv',
+                            encoding = "ISO-8859-1",
+                            dtype={'Div1Airport': str, 'Div1TailNum': str,
+                                   'Div2Airport': str, 'Div2TailNum': str})
+
+
+# List of years
+year_list = [i for i in range(2005, 2020, 1)]
+
+"""Compute graph data for creating yearly airline performance report
+
+Function that takes airline data as input and create 5 dataframes based on the grouping condition to be used for plottling charts and grphs.
+
+Argument:
+
+    df: Filtered dataframe
+
+Returns:
+   Dataframes to create graph.
+"""
+def compute_data_choice_1(df):
+    # Cancellation Category Count
+    bar_data = df.groupby(['Quarter','CancellationCode'])['Flights'].sum().reset_index()
+    # Average flight time by reporting airline
+    line_data = df.groupby(['Quarter','Reporting_Airline'])['AirTime'].mean().reset_index()
+    # Diverted Airport Landings
+    div_data = df[df['DivAirportLandings'] != 0.0]
+    # Source state count
+    map_data = df.groupby(['OriginState'])['Flights'].sum().reset_index()
+    # Destination state count
+    tree_data = df.groupby(['DestState', 'Reporting_Airline'])['Flights'].sum().reset_index()
+    return bar_data, line_data, div_data, map_data, tree_data
+
+
+"""Compute graph data for creating yearly airline delay report
+
+This function takes in airline data and selected year as an input and performs computation for creating charts and plots.
+
+Arguments:
+    df: Input airline data.
+
+Returns:
+    Computed average dataframes for carrier delay, weather delay, NAS delay, security delay, and late aircraft delay.
+"""
+def compute_data_choice_2(df):
+    # Compute delay averages
+    avg_car = df.groupby(['Quarter','Reporting_Airline'])['CarrierDelay'].mean().reset_index()
+    avg_weather = df.groupby(['Quarter','Reporting_Airline'])['WeatherDelay'].mean().reset_index()
+    avg_NAS = df.groupby(['Quarter','Reporting_Airline'])['NASDelay'].mean().reset_index()
+    avg_sec = df.groupby(['Quarter','Reporting_Airline'])['SecurityDelay'].mean().reset_index()
+    avg_late = df.groupby(['Quarter','Reporting_Airline'])['LateAircraftDelay'].mean().reset_index()
+    return avg_car, avg_weather, avg_NAS, avg_sec, avg_late
+
+
+# Application layout
+app.layout = html.Div(children=[
+                                # TODO1: Add title to the dashboard
+                              html.H1('US Domestic Airline Flights Performance',
+                                         style={'textAlign': 'center',
+                                                'color': '#503D36',
+                                                 'font-size': 40}),
+
+
+
+                                html.Div([
+                                    # Add an division
+                                    html.Div([
+
+                                        html.Div(
+                                            [
+                                            html.H2('Report Type:', style={'margin-right': '2em'}),
+                                            ]
+                                        ),
+                                        # TODO2: Add a dropdown
+
+                                       dcc.Dropdown(id='input-type',
+                                                     # Update dropdown values using list comphrehension
+                                                     options=[{'label':'Yearly Airline Performance Report', 'value':'OPT1'},{'label':'Yearly Average Flight Delay Statistics', 'value':'OPT2'}],
+                                                     placeholder="Report Type",
+                                                     style={'width':'80%', 'padding':'3px', 'font-size': '20px', 'text-align-last' : 'center'}),
+                                            # Place them next to each other using the division style
+
+
+
+                                    # Place them next to each other using the division style
+                                    ], style={'display':'flex'}),
+
+                                   # Add next division
+                                   html.Div([
+                                       # Create an division for adding dropdown helper text for choosing year
+                                        html.Div(
+                                            [
+                                            html.H2('Choose Year:', style={'margin-right': '2em'})
+                                            ]
+                                        ),
+
+                                        dcc.Dropdown(id='input-year',
+                                                     # Update dropdown values using list comphrehension
+                                                     options=[{'label': i, 'value': i} for i in year_list],
+                                                     placeholder="Select a year",
+                                                     style={'width':'80%', 'padding':'3px', 'font-size': '20px', 'text-align-last' : 'center'}),
+
+
+                                     ], style={'display': 'flex'}) ,
+
+                                    html.Br(),
+                                html.Br(),
+                                     ]),
+                                # Add Computed graphs
+                                # REVIEW3: Observe how we add an empty division and providing an id that will be updated during callback
+                                html.Div(html.Div([ ], id='plot1')),
+
+                                html.Div([
+                                        html.Div([ ], id='plot2'),
+                                        html.Div([ ], id='plot3')]
+                                , style={'display': 'flex'}),
+
+                                html.Div([
+                                           html.Div([], id='plot4'),
+                                           html.Div([], id='plot5')
+                                         ]
+                                , style={'display': 'flex'}),
+
+
+])
+
+
+ #Callback function definition
+# TODO4: Add 5 ouput components
+@app.callback( [Output(component_id='plot1', component_property='children'),
+               Output(component_id='plot2', component_property='children'),
+               Output(component_id='plot3', component_property='children'),
+               Output(component_id='plot4', component_property='children'),
+               Output(component_id='plot5', component_property='children')],
+               [Input(component_id='input-type', component_property='value'),
+                Input(component_id='input-year', component_property='value')],
+               # REVIEW4: Holding output state till user enters all the form information. In this case, it will be chart type and year
+               [State('plot1', 'children'), State('plot2', 'children'),
+                State('plot3', 'children'), State('plot4', 'children'),
+                State('plot5', 'children')
+               ])
+# Add computation to callback function and return graph3lk43:
+def get_graph(chart, year, children1, children2, c3, c4, c5):
+
+        # Select data
+        df =  airline_data[(airline_data['Quarter']==1)| (airline_data['Quarter']==2)|( airline_data['Quarter']==3)|( airline_data['Quarter']==4)]
+        df.replace(["UA","AA","US","F9","B6","OO","AS","NK","WN","DL","EV","HA","MQ","VX","9E","G4","OH"],
+            ["United Air Lines Inc.","American Airlines Inc.","US Airways Inc.",
+             "Frontier Airlines Inc.","JetBlue Airways","Skywest Airlines Inc.",
+             "Alaska Airlines Inc.","Spirit Air Lines","Southwest Airlines Co.",
+             "Delta Air Lines Inc.","Atlantic Southeast Airlines","Hawaiian Airlines Inc.",
+             "American Eagle Airlines Inc.","Virgin America","Endeavor Air","Allegiant Air","PSA Airlines"], inplace=True)
+
+        if chart == 'OPT1':
+            # Compute required information for creating graph from the data
+            bar_data, line_data, div_data, map_data, tree_data = compute_data_choice_1(df)
+             # Number of flights under different cancellation categories
+            bar_fig = px.bar(bar_data, x='Quarter', y='Flights', color='CancellationCode', title='Quarterly Flight Cancellation')
+
+            # TODO5: Average flight time by reporting airline
+            # TODO5: Average flight time by reporting airline
+        #line_fig = px.line(line_data, x='Quarter', y='AirTime', color='Reporting_Airline', title='Average quarterly flight time (minutes) by airline')`
+
+            line_fig = px.line(line_data, x='Quarter', y='AirTime', color='Reporting_Airline', title='Average Quarterly Flight Time (minutes) by Airline')
+
+
+            # Percentage of diverted airport landings per reporting airline
+            pie_fig = px.pie(div_data, values='Flights', names='Reporting_Airline', title='% of Flights by Reporting Airline')
+
+
+            map_fig = px.choropleth(map_data,  # Input data
+                    locations='OriginState',
+                    color='Flights',
+                    hover_data=['OriginState', 'Flights'],
+                    locationmode = 'USA-states', # Set to plot as US States
+                    color_continuous_scale='GnBu',
+                    range_color=[0, map_data['Flights'].max()])
+            map_fig.update_layout(
+                    title_text = 'Number of Flights From Origin State',
+                    geo_scope='usa') # Plot only the USA instead of globe
+             # TODO6: Number of flights flying to each state from each reporting airline
+            tree_fig=px.treemap(tree_data, path=['DestState', 'Reporting_Airline'], values='Flights', color='Flights',color_continuous_scale='RdBu', title='Flight Count by Airline to Destination State')
+
+
+            # REVIEW6: Return dcc.Graph component to the empty division
+            return [dcc.Graph(figure=tree_fig),
+                    dcc.Graph(figure=pie_fig),
+                    dcc.Graph(figure=map_fig),
+                    dcc.Graph(figure=bar_fig),
+                    dcc.Graph(figure=line_fig)
+
+                   ]
+        else:
+            # REVIEW7: This covers chart type 2 and we have completed this exercise under Flight Delay Time Statistics Dashboard section
+            # Compute required information for creating graph from the data
+            avg_car, avg_weather, avg_NAS, avg_sec, avg_late = compute_data_choice_2(df)
+
+            # Create graph
+
+            carrier_fig = px.line(avg_car, x='Quarter', y='CarrierDelay', color='Reporting_Airline', title='Average Carrier Delay Time (minutes) by Airline')
+            weather_fig = px.line(avg_weather, x='Quarter', y='WeatherDelay', color='Reporting_Airline', title='Average Weather Delay Time (minutes) by Airline')
+            nas_fig = px.line(avg_NAS, x='Quarter', y='NASDelay', color='Reporting_Airline', title='Average NAS Delay Time (minutes) by Airline')
+            sec_fig = px.line(avg_sec, x='Quarter', y='SecurityDelay', color='Reporting_Airline', title='Average Security Delay Time (minutes) by Airline')
+            late_fig = px.line(avg_late, x='Quarter', y='LateAircraftDelay', color='Reporting_Airline', title='Average Late Aircraft Delay Time (minutes) by Airline')
+
+            return[dcc.Graph(figure=carrier_fig),
+                   dcc.Graph(figure=weather_fig),
+                   dcc.Graph(figure=nas_fig),
+                   dcc.Graph(figure=sec_fig),
+                   dcc.Graph(figure=late_fig)]
+
+
+# Run the app
+if __name__ == '__main__':
+    # REVIEW8: Adding dev_tools_ui=False, dev_tools_props_check=False can prevent error appearing before calling callback function
+    app.run_server(mode="inline", host="localhost", debug=False, dev_tools_ui=False, dev_tools_props_check=False)
